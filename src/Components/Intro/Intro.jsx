1750c292227706f0c89231efab170aa63df21c93
Edvin <85315080+OnlyPositiveViibes@users.noreply.github.com>
2022-01-17 21:40:10 +0200

    Fix nr2

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
             <div className="i-left">
             <div className="i-left-wrapper">
                 <h2 className="i-intro">Hello, My name is</h2>
-                <h1 className="i-name">Edvinas Ramanauskas</h1>
+                <h3 className="i-name">Edvinas Ramanauskas</h3>
                 <div className="i-title">
                     <div className="i-title-wrapper">
                         <div className="i-title-item">Junior Full-Stack developer</div>
