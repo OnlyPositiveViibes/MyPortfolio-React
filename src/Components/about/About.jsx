250400bcb36efd34c1c2645402962327e821efc8
Edvin <85315080+OnlyPositiveViibes@users.noreply.github.com>
2022-01-17 21:31:44 +0200

    Fix

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -17,16 +17,13 @@ const About = () => {
 
             <div className="a-right">
                 <h1 className="a-title">About Me</h1>
-                <p className="a-sub">
-                    Hey once again i'm brand new Junior Full-Stack developer
-                </p>
                 <p className="a-desc">
-                In school times I was very addicted to programming and was wondering one day become my own programming company CEO, but one day
-                    my wonders switched 180 degrees after my final exams I got called to volunteer in the Lithuania military and after that
+                    Hey once again i'm brand new Junior Full-Stack developer
+                    My wonders switched 180 degrees after my final exams I got called to volunteer in the Lithuania military and after that
                     my plans changed. After volunteering in the military I decided to try to find myself so I took GAP years after those years
                     I decided to join the programmers' crew very spontaneous, one day my mother told me that she applied for studies in one of the programming institutes
                     and that news hit me hard as the wall, so I told to myself I had to try it once again and I LOVED IT I found myself in this profession after years of trying a lot of other professions.
-                    More about me I love to race with cartings, take photos of everything read books and travel, for now, I think that's enough if I'll met your requirements you'll get more details about me
+                    More about me I love to race with cartings, take photos of everything read books and travel, for now,
                 </p>
                 <div className="a-award">
                     <img src={Award} alt="" className="a-award-img" />
